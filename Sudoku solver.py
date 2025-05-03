e4d42c0a7777eb75a9780ab96f04f82a2ab88cca
Mkhaled4 <mohnabil489@gmail.com>
2025-05-04 01:49:09 +0200

    print grid of 1s

---
+++ Sudoku solver.py
@@ -0,0 +1,12 @@
+rows, columns = 3, 3
+row = []
+grid=[]
+num=0
+for r in range(rows):
+    for c in range(columns):
+        row = num+1
+    grid.append(row)
+
+for r in range(rows):
+    print(grid)
+
