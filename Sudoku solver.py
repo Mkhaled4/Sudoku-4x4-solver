962314de4cad864ed44c51112993582e02207dbe
Mkhaled4 <mohnabil489@gmail.com>
2025-05-04 02:10:16 +0200

    printing grid from 1 to 9

--- Sudoku solver.py
+++ Sudoku solver.py
@@ -4,9 +4,10 @@ grid=[]
 num=0
 for r in range(rows):
     for c in range(columns):
-        row = num+1
-    grid.append(row)
+        num += 1
+        row.append(num)
+    print(row)
+    row=[]
 
-for r in range(rows):
-    print(grid)
+
 
