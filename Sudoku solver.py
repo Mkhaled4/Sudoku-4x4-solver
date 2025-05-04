1e59abb95324836603a0ccde2c24fd16880bc8c5
Mkhaled4 <mohnabil489@gmail.com>
2025-05-04 14:35:53 +0200

    yes/no conflict

--- Sudoku solver.py
+++ Sudoku solver.py
@@ -1,9 +1,27 @@
+#initialises the grid and its structure
 rows, columns = 3, 3
 grid = []
 
+#takes input of the box
 for r in range(rows):
-    row = list(map(int, input().split()))
+    number=input()
+    row = list(map(int,number))
     grid.append(row)
 
+
+x,y=2,0
+
+
 for row in grid:
+    compare = grid[x][y]
+    for n in row:
+       if n == compare:
+           print("yes")
+       else:
+           print("no")
     print(row)
+    y += 1
+    print (y)
+    print(compare)
+    print (x)
+
