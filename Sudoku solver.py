70a1a8dc2e5729800b5ef285afb63bf94bfaeba1
Mkhaled4 <mohnabil489@gmail.com>
2025-05-04 20:23:52 +0200

    filling in last number in row

--- Sudoku solver.py
+++ Sudoku solver.py
@@ -1,5 +1,5 @@
 def check(lst):
-    required= set(range(1, 10))
+    required= set(range(1, 5))
     input_set = set(lst)
     missing_numbers = required - input_set
     if missing_numbers:
@@ -7,29 +7,48 @@ def check(lst):
     else:
        return True
 
+def solv1(lst,y):
+    required = [1, 2, 3, 4]
+    input_set = lst
+    for item in required:
+        if not item in input_set:
+            input_set[y]= item
+            return input_set
 
 
-
-
-#initialises the grid and its structure
-rows, columns = 2,9
+rows, columns = 4,4
 grid = []
 
-#takes input of the box
+
 for r in range(rows):
     number=input()
     new= number.replace(" ","0")
-    print(new)
     row = list(map(int, list(new)))
 
     grid.append(row)
 
-print(grid)
+
 
 for row in grid:
     result=check(row)
-    print(result)
 
-for columns in grid:
-    result=check(columns)
-    print(result)
+
+
+column = []
+for col_index in range(4):
+    indcol=[]
+    for row_index in range(4):
+        indcol.append(grid[row_index][col_index])
+    column.append(indcol)
+
+for row in column:
+    result=check(row)
+
+
+for x in range(len(grid)):
+    for y in range(len(grid[0])):
+        if grid[x].count(0) == 1:
+           grid[x][y]= 0
+           grid[x] = solv1(grid[x],y)
+
+print(grid)
